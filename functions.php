810801154787f261989bccfa89f405dc25414adb
Rose <rose1773@stud.kea.dk>
2023-11-09 23:25:14 +0100

    childsecond

--- functions.php
+++ functions.php
@@ -5,10 +5,7 @@ function hello_elementor_child_scripts_styles() {
 		'hello-elementor-child-style',
 		get_stylesheet_directory_uri() . '/style.css',
 		[
-			'hello-elementor-theme-style',
+			'hello-elementor-theme-style', 
 		],
-		HELLO_ELEMENTOR_CHILD_VERSION
-	);
-
 }
 add_action( 'wp_enqueue_scripts', 'hello_elementor_child_scripts_styles', 20 );
