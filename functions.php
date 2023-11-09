47cd3bf444c845f5afe9e077a54713d23fc8146c
Rose <rose1773@stud.kea.dk>
2023-11-09 23:11:14 +0100

    helloelementor-childtheme

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+function hello_elementor_child_scripts_styles() {
+
+	wp_enqueue_style(
+		'hello-elementor-child-style',
+		get_stylesheet_directory_uri() . '/style.css',
+		[
+			'hello-elementor-theme-style',
+		],
+		HELLO_ELEMENTOR_CHILD_VERSION
+	);
+
+}
+add_action( 'wp_enqueue_scripts', 'hello_elementor_child_scripts_styles', 20 );
